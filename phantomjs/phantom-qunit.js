d99b55ba7d0f23b426f8b478504a2c8c7041edaa
Adrian Adkison <adrian@surveymonkey.com>
2012-10-07 18:49:03 -0700

    added qunit example

--- phantomjs/phantom-qunit.js
+++ phantomjs/phantom-qunit.js
@@ -2,13 +2,12 @@ var system = require('system');
 var webPage = require('webpage').create();
 var scriptName = system.args[0];
 
-
 if(system.args.length != 2){
     console.log("Usage: phantomjs " + scriptName + " <url>");
     phantom.exit(1);
 }
 
-var testUrl = system.args[1];
+var testFile = system.args[1];
 
 //if there is a javascript in the webpage runtime
 webPage.onError = function(msg, trace){
@@ -25,44 +24,38 @@ webPage.onConsoleMessage = function(msg){
     console.log(msg);
 };
 
-
 webPage.viewPortSize = {
     width: 980,
     height: 1000
 };
 
-webPage.open( url, function(status){
+webPage.open( testFile, function(status){
 
     //if the web page opens successfully lets do some exporting
     //otherwise lets exit phantomjs
     if ( status !== 'success') {
-        console.log("Unable to load " + url);
+        console.log("Unable to load " + testFile);
         phantom.exit(2);
     }
 
     setInterval(function() {
-        isComplete = webPage.evaluate(function (e) {
+        var isComplete = webPage.evaluate(function (e) {
             var bannerClasses = document.getElementById('qunit-banner').className;
-            var hasFailedClass = bannerClasses.indexOf('qunit-fail') !== -1;
-            var hasPassedClass = bannerClasses.indexOf('qunit-pass') !== -1;
+            var failed = bannerClasses.indexOf('qunit-fail') !== -1;
+            var passed = bannerClasses.indexOf('qunit-pass') !== -1;
 
-            if( !hasFailedClass && !hasFailedClass ){
-                return false;
-            }else if(hasFailedClass){
-                return "failed";
-            } else {
-                return "passed";
-            }
-
+            if( !failed && !passed ) return false;
+            if(failed) return "failed";
+            return "passed";
         });
 
         if(isComplete){
             if(isComplete === "failed"){
-                console.log("Tests failed");
+                console.log("Failed. You should probably drink a beer");
                 phantom.exit(3);
             }
 
-            console.log("GGGEEEYYYAAAAAH BOYYY!");
+            console.log("Passed. You should probably drink a beer");
             phantom.exit(0);
         }
     }, 100);
